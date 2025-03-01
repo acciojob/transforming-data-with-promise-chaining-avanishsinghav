2add12a79128064c5ff19cf777e2fc068c43e2d8
Avanish Singh <107432071+avanishsinghav@users.noreply.github.com>
2025-03-01 11:27:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", ()=>{
+	let Num = parseFloat(document.getElementById("ip").value);
+	new Promise((resolve) =>{
+		setTimeout(()=>{
+			document.getElementById("output").innerText = `Result:  ${Num}`;
+			resolve(Num);
+		},2000)
+	})
+	.then((value)=>
+		new Promise((resolve)=>{
+			setTimeout(()=>{
+				let result = value*2;
+				document.getElementById("output").innerText = `Result: ${result}`;
+				resolve(result);
+			},2000)
+		}))
+	    .then((value) =>
+			new Promise((resolve)=>{
+				setTimeout(()=>{
+					let result = value - 3;
+					document.getElementById("output").innerText = `Result: ${result}`;
+					resolve(result);
+				},1000)
+			})
+		)
+	    .then((value) =>
+			new Promise((resolve)=>{
+				setTimeout(()=>{
+					let result = value /2;
+					document.getElementById("output").innerText = `Result: ${result}`;
+					resolve(result);
+				},1000)
+			})
+		)
+	   .then((value) =>
+			new Promise((resolve)=>{
+				setTimeout(()=>{
+					let result = value + 10;
+					document.getElementById("output").innerText = `Result: ${result}`;
+					resolve(result);
+				},1000)
+			})
+		)
+})
